9482c35c0a3377060624a42f019c74ed1279ab23
Krisztina Somogyi <somogyikrisztina01@gmail.com>
2026-04-22 18:12:51 +0300

    Flake8 - 4

--- app/main.py
+++ app/main.py
@@ -29,10 +29,10 @@ class CarWashStation:
             return 0.0
 
         price = (
-                car.comfort_class
-                * (self.clean_power - car.clean_mark)
-                * self.average_rating
-                / self.distance_from_city_center
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
         )
         return round(price, 1)
 
